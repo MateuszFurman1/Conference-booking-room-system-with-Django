73d9a31d8a6cdb2a3dcef80d6abed090626bf835
Mateusz <fur.mateusz@gmail.com>
2022-11-14 21:26:41 +0100

    view test add

--- booking_rooms_app/conftest.py
+++ booking_rooms_app/conftest.py
@@ -1,22 +1,29 @@
 import pytest
 from django.contrib.auth.models import User, Permission
 
-from shelf.models import Person, Studio, Movie
+from booking_rooms_app.models import Room
 
 
 @pytest.fixture
-def oneMovie(persons):
-    p = Movie.objects.create(title='Owoc który się nie kula', year=2022, director=persons[0])
-    return p
+def rooms():
+    lst = []
+    for n in range(10):
+        p = Room.objects.create(name=n, seats=n, projector=False, created='2022-11-07 20:20:46.816306+01',
+                                updated='2022-11-07 20:20:46.816306+01')
+        lst.append(p)
+    return lst
 
-@pytest.fixture
-def user_with_permission():
-    u = User.objects.create(username='tadeusz')
-    permission = Permission.objects.get(codename='add_comment')
-    u.user_permissions.add(permission)
-    return u
-
+# @pytest.fixture
+# def oneMovie(persons):
+#     p = Movie.objects.create(title='Owoc który się nie kula', year=2022, director=persons[0])
+#     return p
+#
 @pytest.fixture
 def user():
-    u = User.objects.create(username='amadeusz')
-    return u
+    return User.objects.create(username='tadeusz')
+
+
+# @pytest.fixture
+# def user():
+#     u = User.objects.create(username='amadeusz')
+#     return u
--- booking_rooms_app/tests.py
+++ booking_rooms_app/tests.py
@@ -1,15 +1,75 @@
+from datetime import datetime
+
 import pytest
 from django.contrib.auth.models import User
 from django.test import Client
 from django.urls import reverse
 
+from booking_rooms_app.form import RoomForm
+from booking_rooms_app.models import Room
+
 
 @pytest.mark.django_db
-def test_Allpersons_view(persons):
+def test_create_room_get_view(user):
+    client = Client()
+    url = reverse('create-room')
+    client.force_login(user)
+    response = client.get(url)
+    assert 200 == response.status_code
+    assert isinstance(response.context['form'], RoomForm)
+
+
+@pytest.mark.django_db
+def test_create_room_post_view_with_log(user):
+    client = Client()
+    url = reverse('create-room')
+    client.force_login(user)
+    data = {
+        'name': 'room',
+        'seats': 100,
+        'projector': False
+        }
+    response = client.post(url, data)
+    assert response.status_code == 302
+    assert Room.objects.get(name='room', seats=100, projector=False)
+
+
+@pytest.mark.django_db
+def test_Allrooms_view(rooms):
     client = Client()  # otwieramt przeglądarkę
-    url = reverse('all-persons')  # mówimy na jaki url chcemy wejsc
+    url = reverse('all-rooms')  # mówimy na jaki url chcemy wejsc
     response = client.get(url)  # wchodzimu na url
-    persons_context = response.context['persons']
-    assert persons_context.count() == len(persons)
-    for p in persons:
+    persons_context = response.context['rooms']
+    assert persons_context.count() == len(rooms)
+    for p in rooms:
         assert p in persons_context
+
+
+@pytest.mark.django_db
+def test_edit_room_get_view(user, rooms):
+    client = Client()
+    url = reverse('edit-room')
+    client.force_login(user)
+    data = {
+        'name': 'room',
+        'seats': 100,
+        'projector': False
+    }
+    response = client.get(url, data)
+    # assert 200 == response.status_code
+    assert isinstance(response.context['form'], RoomForm)
+
+
+@pytest.mark.django_db
+def test_edit_room_post_view_with_perm(user):
+    client = Client()
+    url = reverse('edit-room')
+    client.force_login(user)
+    data = {
+        'name': 'room',
+        'seats': 100,
+        'projector': False
+        }
+    response = client.post(url, data)
+    assert response.status_code == 302
+    assert Room.objects.get(name='room', seats=100, projector=False)
